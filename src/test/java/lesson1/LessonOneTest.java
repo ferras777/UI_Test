c684e2ad1c36f70b374d253e0f33fb1035a65aca
Ilya Malykh <ferras2007@yandex.ru>
2020-07-29 16:49:52 +0300

    Added Testcase0003

--- src/test/java/lesson1/LessonOneTest.java
+++ src/test/java/lesson1/LessonOneTest.java
@@ -1,9 +1,6 @@
 package lesson1;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.NoSuchFrameException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -39,6 +36,7 @@ public class LessonOneTest {
 
         // 1. Navigate aliexpress
         driver.navigate().to("https://aliexpress.ru/");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
 
         // 2. Close advertisement
         try {
@@ -51,22 +49,26 @@ public class LessonOneTest {
         driver.findElement(By.linkText("Войти")).click();
 
         // 4. Wait 5 sec for load frame
-        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
 
         // 5. Switch to frame
+
         try {
-            driver.switchTo().frame(1);
+            driver.switchTo().frame("alibaba-login-box");
         } catch (NoSuchFrameException e) {
-            System.out.print("No frame");
+            System.out.print("Can't find frame");
         }
 
         // 6. Enter login
-        driver.findElement(By.id("fm-login-id")).sendKeys(login);
+//        driver.findElement(By.id("fm-login-id")).sendKeys(login);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/form/div[1]/div/input")).sendKeys(login);
 
         // 7. Enter password
         driver.findElement(By.id("fm-login-password")).sendKeys(password);
 
         // 8. Click submit button
-        driver.findElement(By.className("fm-button fm-submit password-login")).click();
+        driver.findElement(By.className("password-login")).click();
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+//        driver.findElement(By.xpath("/html/body/div[1]/div[4]/div[1]/div[2]/div/div[4]/div[2]/div[3]/ul/li[3]/a/span[1]")).click();
     }
 }
