dd2508f69531d7e99ee666e4bc6388d2d84e6848
aleksey_krapivnitskiy <krapivnitskiy@secureducation.ru>
2020-08-04 21:09:48 +0300

    added task.txt

--- src/main/pages/NavigationBar.java
+++ src/main/pages/NavigationBar.java
@@ -23,4 +23,8 @@ public class NavigationBar {
     public void fillSearchField(Credentials value) {
         searchField.sendKeys(value.login);
     }
+
+    public void openTab(int tabIndex) {
+
+    }
 }
