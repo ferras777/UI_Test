0896b8868dd6a8e399f0c879716ab6345a7b0bd9
ferras <ferras2007@yandex.ru>
2020-08-17 22:53:53 +0300

    Add page Cart

---
+++ src/test/java/lesson1/pages/Cart.java
@@ -0,0 +1,38 @@
+package lesson1.pages;
+
+import lesson1.test.SeleniumBase;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Cart extends SeleniumBase {
+
+    @FindBy(css = ".select-all-container .next-checkbox-input")
+    private WebElement checkBoxAllItemsInCart;
+
+    @FindBy(css = "[ae_button_type=\"remove\"]")
+    private WebElement removeButton;
+
+    @FindBy(css = "div.next-dialog-footer.next-align-left > button:first-child")
+    private WebElement okButton;
+
+    public Cart(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void deleteAllItemsInCart() {
+        try {
+            while (removeButton.isEnabled()) {
+                //noinspection BusyWait
+                Thread.sleep(1000);
+                removeButton.click();
+                okButton.click();
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("Cart is empty");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
