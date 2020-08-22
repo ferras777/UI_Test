3b4331f454269ac3390a0ffcb3c65b786314c8da
Ilya <ferras2007@yandex.ru>
2020-08-22 18:31:28 +0300

    Add MainPage

---
+++ src/test/java/lesson1/pages/selenidePages/MainPage.java
@@ -0,0 +1,14 @@
+package lesson1.pages.selenidePages;
+
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+public class MainPage {
+
+    public ElementsCollection getBannersCollection() {
+        return $$(".ui-banner-slider-nav > span");
+    }
+
+
+}
