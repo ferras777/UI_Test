e795fb76c616970b5511907bdea7105bd7c6f0d8
Ilya <ferras2007@yandex.ru>
2020-08-24 15:02:00 +0300

    Make webdriver static, clean up code

--- src/test/java/lesson1/test/SeleniumBase.java
+++ src/test/java/lesson1/test/SeleniumBase.java
@@ -1,20 +1,17 @@
 package lesson1.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 public abstract class SeleniumBase {
-    public WebDriver driver;
+    public static WebDriver driver;
     public WebDriverWait wait;
 
     public String SITE_URL = "https://aliexpress.ru/";
@@ -35,17 +32,9 @@ public abstract class SeleniumBase {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @AfterClass
+    @AfterMethod
     public void afterClass() {
         driver.close();
     }
 
-    public WebElement element(String cssSelector) {
-        waitVisibilityOfElement(cssSelector);
-        return driver.findElement(By.cssSelector(cssSelector));
-    }
-
-    public void waitVisibilityOfElement(String cssSelector) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(cssSelector)));
-    }
 }
