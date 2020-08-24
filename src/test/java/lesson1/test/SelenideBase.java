a75a91e172cd8fbd8ef4e2136da2dd7e4c3c9869
Ilya <ferras2007@yandex.ru>
2020-08-24 15:00:59 +0300

    Refactor

--- src/test/java/lesson1/test/SelenideBase.java
+++ src/test/java/lesson1/test/SelenideBase.java
@@ -6,10 +6,9 @@ import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 
-public class SelenideBase {
+public abstract class SelenideBase {
     public String SITE_URL = "https://aliexpress.ru/";
-    public String pathToProfile = "--user-data-dir=src\\main\\resources\\Profiles";
-    public String profile = "--profile-directory=Profile 1";
+
 
     @BeforeClass
     public void beforeClass() {
@@ -18,10 +17,18 @@ public class SelenideBase {
         Configuration.baseUrl = SITE_URL;
         Configuration.startMaximized = true;
 
+
+        Configuration.browserCapabilities = new MutableCapabilities(profileForAuthorizationChrome());
+    }
+
+    public ChromeOptions profileForAuthorizationChrome() {
+        String pathToProfiles = "--user-data-dir=src\\main\\resources\\Profiles";
+        String profile = "--profile-directory=Profile 1";
         ChromeOptions options = new ChromeOptions();
-        options.addArguments(pathToProfile);
+
+        options.addArguments(pathToProfiles);
         options.addArguments(profile);
 
-        Configuration.browserCapabilities = new MutableCapabilities(options);
+        return options;
     }
 }
