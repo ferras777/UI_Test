7a9ca669cc1150f0a136104e290a00898d038222
Ilya <ferras2007@yandex.ru>
2020-08-25 21:03:48 +0300

    Add CategoryItemsRightSizeTest

---
+++ src/test/java/lesson1/tests/selenide/CategoryItemsRightSizeTest.java
@@ -0,0 +1,36 @@
+package lesson1.tests.selenide;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.SelenideElement;
+import lesson1.pages.selenidePages.MainPage;
+import lesson1.test.SelenideBase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class CategoryItemsRightSizeTest extends SelenideBase {
+    MainPage mainPage = new MainPage();
+
+    @BeforeMethod
+    public void beforeMethod() {
+        open("/");
+    }
+
+    @Test
+    public void categoryItemsRightSizeTest() {
+        // Click in category folder
+        mainPage.clickOnCategoriesFolders();
+
+        // Hover all categories, for load items
+        for (SelenideElement element : mainPage.getMainMenuElements()) {
+            element.hover();
+            sleep(700);
+        }
+        // Checks size of menu elements
+        mainPage.getMainMenuElements().shouldBe(CollectionCondition.size(13));
+        mainPage.getSecondMenuBoldElements().shouldHave(CollectionCondition.size(82));
+        mainPage.getSecondMenuNormalElements().shouldHave(CollectionCondition.size(588));
+    }
+}
