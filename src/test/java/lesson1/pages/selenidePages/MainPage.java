1e8fbc04a1e5aa62cb2cc5ef676a71bc433f190c
Ilya <ferras2007@yandex.ru>
2020-08-25 21:03:26 +0300

    Add mainPage categories collections

--- src/test/java/lesson1/pages/selenidePages/MainPage.java
+++ src/test/java/lesson1/pages/selenidePages/MainPage.java
@@ -2,9 +2,26 @@ package lesson1.pages.selenidePages;
 
 import com.codeborne.selenide.ElementsCollection;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class MainPage {
+
+    public void clickOnCategoriesFolders() {
+        $(".categories-content-title > span:first-child").click();
+    }
+
+    public ElementsCollection getMainMenuElements() {
+        return $$(".cl-item > dt");
+    }
+
+    public ElementsCollection getSecondMenuBoldElements() {
+        return $$(".sub-cate-items > dt > a");
+    }
+
+    public ElementsCollection getSecondMenuNormalElements() {
+        return $$(".sub-cate-items > dd > a");
+    }
 
     public ElementsCollection getBannersCollection() {
         return $$(".ui-banner-slider-nav > span");
